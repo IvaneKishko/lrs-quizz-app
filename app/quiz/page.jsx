9a982cfec6a970c1b7dc505c7555be4fe3f94c7b
Ivane Kishko <ivanekishko@Ivanes-MacBook-Air.local>
2024-03-26 18:18:26 +0400

    update content

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -69,7 +69,7 @@ const page = () => {
                 className="bg-blue-500 text-white font-bold py-2 px-4 mt-2 rounded hover:bg-blue-700"
                 onClick={nextQuestion}
               >
-                Next
+                Next-
               </button>
             ) : (
               <button
